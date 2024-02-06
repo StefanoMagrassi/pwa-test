6e8992bdc7cecff05a4285ca7c9276c92370c7f3
StefanoMagrassi <1291271+StefanoMagrassi@users.noreply.github.com>
2024-02-06 17:31:52 +0100

    remove auth0

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,4 @@
-import * as T from 'fp-ts/Task';
-import {pipe} from 'fp-ts/function';
-import {runAuth0} from './auth0';
 import {detect} from './ua';
 
 // --- Run
-pipe(
-  runAuth0,
-  T.chainIOK(() => detect)
-)().catch(console.error);
+detect();
