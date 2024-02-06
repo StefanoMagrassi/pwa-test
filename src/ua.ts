95d4f6a3b0653c70eebb15ce1a7a54497499b00d
StefanoMagrassi <1291271+StefanoMagrassi@users.noreply.github.com>
2024-02-06 18:00:08 +0100

    chore

--- src/ua.ts
+++ src/ua.ts
@@ -1,5 +1,5 @@
 import * as IO from 'fp-ts/IO';
-import {UAParser, type IResult} from 'ua-parser-js';
+import {UAParser, type IResult} from 'ua-parser-js'; // ref. https://github.com/faisalman/ua-parser-js/issues/680
 
 export const detect: IO.IO<IResult> = () => {
   const uaParser = new UAParser();
@@ -7,5 +7,6 @@ export const detect: IO.IO<IResult> = () => {
   return uaParser.getResult();
 };
 
+// ref. https://github.com/faisalman/ua-parser-js/issues/182
 export const isDesktop = (ua: IResult): boolean =>
   typeof ua.device.type === 'undefined';
